8187ced098a74ba72d22711265422498af5bb33e
ardietmd <ardietachmad@gmail.com>
2021-04-20 20:09:13 +0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://covid19.mathdro.id/api/countries/Indonesia')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        isLoaded: true,
+        items: json,
+      })
+    });
+  }
+
+  render() {
+
+    const { isLoaded, items} = this.state;
+
+    if(!isLoaded) {
+      return <div>Loading....</div>
+    }
+    else {
+      return(
+        <div className="App">
+        <div className="Title"> 
+          <h1>Data covid 19</h1>
+          </div>
+
+            <ul>
+              <li> Confirmed : {items.confirmed.value}</li>
+              <li> Cecovered : {items.recovered.value}</li>
+              <li> Deaths : {items.deaths.value}</li>
+          
+              
+            </ul>
+            
+        </div>
+      );
+    }
+    
+  }
 }
 
 export default App;
